c326d18ec90c8a01387274ff290c379b690a7126
Jordan Howard <jordanhoward1991@gmail.com>
2024-12-12 15:41:19 -0700

    adds basic sawtooth site

---
+++ sawtooth-outdoors/scripts/main.js
@@ -0,0 +1,29 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const heroButton = document.querySelector('.hero button');
+    
+    heroButton.addEventListener('click', () => {
+        alert('Adventure awaits! Let\'s explore the Sawtooth Mountains!');
+    });
+
+    // Get the current year
+	var currentYear = new Date().getFullYear();
+
+	// Get the last modified date of the document
+	var lastModifiedDate = document.lastModified;
+
+	// Update the copyright year in the footer's first paragraph
+	document.querySelector("footer p:first-child span").textContent = currentYear;
+
+	// Update the last modified date in the footer's second paragraph
+	document.getElementById("lastModified").textContent = "Last Modified: " + lastModifiedDate;
+
+	// Hamburger Menu Toggle for Small Screens
+	const hamburgerButton = document.getElementById("hamburger-menu");
+	const navLinks = document.getElementById("nav-links");
+
+	hamburgerButton.addEventListener("click", () => {
+		// Toggle the "active" class to show/hide the nav links
+		document.querySelector("nav").classList.toggle("active");
+	});
+
+});
